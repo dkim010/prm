390a673a16bd0febf302f37ba8a8149e25fcdbdb
Dongwon Kim <dkim010@gmail.com>
2024-05-17 19:49:52 +0900

    add grafana dashboard for visualization
    Signed-off-by: Dongwon Kim <dkim010@gmail.com>

--- README.md
+++ README.md
@@ -41,3 +41,48 @@ $ python3 prm.py -n slack -i 10 -d 5 -s
                           The output CSV file path (default: 20230327_012916.csv)
     -s, --silent          No stdout (default: False)
   ```
+
+# Visualization with Grafana for MacOS
+- MySQL & Grafana
+  ```bash
+  $ brew update
+  $ brew install mysql grafana
+  $ brew services start mysql
+  $ brew services start grafana
+  ```
+- Python Packages
+  ```bash
+  $ pip install 'polars[sqlalchemy]==0.20.26' 'pymysql==1.1.0' 'pandas>=1.5' 'pyarrow>=8.0.0'
+  ```
+- Insert Logs into MySQL
+  ```bash
+  $ python ingest.py --help
+  usage: ingest.py [-h] -f LOG_FILE -l LOG_NAME -n EXP_NAME -V VERSION [-H MYSQL_HOST] [-p MYSQL_PORT] [-u MYSQL_USER] [-d MYSQL_DATABASE] [-t MYSQL_TABLE]
+
+  optional arguments:
+    -h, --help            show this help message and exit
+    -f LOG_FILE, --log_file LOG_FILE
+                          Log file path (CSV) (default: None)
+    -l LOG_NAME, --log_name LOG_NAME
+                          Log name (default: None)
+    -n EXP_NAME, --exp_name EXP_NAME
+                          Experiment name (default: None)
+    -V VERSION, --version VERSION
+                          Version (default: None)
+    -H MYSQL_HOST, --mysql_host MYSQL_HOST
+    -p MYSQL_PORT, --mysql_port MYSQL_PORT
+    -u MYSQL_USER, --mysql_user MYSQL_USER
+    -d MYSQL_DATABASE, --mysql_database MYSQL_DATABASE
+    -t MYSQL_TABLE, --mysql_table MYSQL_TABLE
+  ```
+  - Example
+    ```bash
+    $ python ingest.py --log_file 20240517_181613.csv --version v0.5 --exp_name feature22 --log_name test3
+    ```
+- Grafana Settings
+  - New Datasource: http://localhost:3000/connections/datasources/new
+    ![plot](./res/mysql-datasource.png)
+  - Import Dashboard: http://localhost:3000/dashboard/import
+    [PRM_Dashbaord](./res/PRM_Dashboard-1715942560789.json)
+- Dashboard Screenshot
+  ![plot](./res/dashboard.png)
---
+++ ingest.py
@@ -0,0 +1,118 @@
+# pylint:disable=invalid-name
+from __future__ import annotations
+
+import argparse
+from pathlib import Path
+from pprint import pprint
+
+import polars as pl
+import pymysql
+
+def string_to_bytes(size_str: str) -> int:
+    if not size_str:
+        return 0
+    sizer = {
+        'G': 1024*1024*1024,
+        'M': 1024*1024,
+        'K': 1024,
+    }
+    return int(size_str[:-1]) * sizer[size_str[-1]]
+
+
+class Ingester:
+    def __init__(
+            self,
+            log_file: Path,
+            version: str,
+            exp_name: str,
+            log_name: str,
+            mysql_host: str,
+            mysql_port: int,
+            mysql_user: str,
+            mysql_database: str,
+            mysql_table: str,
+            timezone_hours: int,
+    ):
+        self.log_file = log_file
+        self.version = version
+        self.exp_name = exp_name
+        self.log_name = log_name
+        # mysql
+        self.mysql_host = mysql_host
+        self.mysql_port = mysql_port
+        self.mysql_user = mysql_user
+        self.mysql_database = mysql_database
+        self.mysql_table = mysql_table
+        self.uri = f'mysql+pymysql://{mysql_user}@{mysql_host}:{mysql_port}/{mysql_database}'
+        self.timezone_hours = timezone_hours
+
+    def _generate_dataframe(self) -> pl.DataFrame:
+        df = pl.read_csv(self.log_file)
+        df = df.select(
+            pl.col('stamp').str.to_datetime('%Y-%m-%d %H:%M:%S')
+                .dt.epoch(time_unit='ms').alias('time'),
+            pl.lit(self.version).alias('version'),
+            pl.lit(self.exp_name).alias('exp_name'),
+            pl.lit(str(self.log_file)).alias('log_file'),
+            pl.lit(self.log_name).alias('log_name'),
+            pl.col('pid'),
+            pl.col('name'),
+            pl.col('cpu_percent'),
+            pl.col('average_cpu_percent'),
+            pl.col('mem_usage').map_elements(string_to_bytes, pl.Int64).alias('mem_usage'),
+        )
+        min_time = df['time'].min() + self.timezone_hours * 3600 * 1000 # UTC+9
+        df = df.with_columns(pl.col('time') - min_time)
+        return df
+
+    def _prepare(self):
+        conn = pymysql.connect(
+            host=self.mysql_host,
+            port=self.mysql_port,
+            user=self.mysql_user,
+        )
+        cursor = conn.cursor()
+        cursor.execute(f'CREATE DATABASE IF NOT EXISTS {self.mysql_database}')
+        cursor.close()
+        conn.close()
+
+    def _ingest_to_database(self, df: pl.DataFrame):
+        df.write_database(
+            connection=self.uri,
+            table_name=self.mysql_table,
+            if_table_exists='append',
+        )
+
+    def start(self):
+        self._prepare()
+        df = self._generate_dataframe()
+        print(df)
+        self._ingest_to_database(df)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-f', '--log_file', type=Path, required=True,
+                        help='Log file path (CSV)')
+    parser.add_argument('-l', '--log_name', type=str, required=True,
+                        help='Log name')
+    parser.add_argument('-n', '--exp_name', type=str, required=True,
+                        help='Experiment name')
+    parser.add_argument('-V', '--version', type=str, required=True,
+                        help='Version')
+    parser.add_argument('-H', '--mysql_host', type=str, default='localhost')
+    parser.add_argument('-p', '--mysql_port', type=int, default=3306)
+    parser.add_argument('-u', '--mysql_user', type=str, default='root')
+    parser.add_argument('-d', '--mysql_database', type=str, default='log_db')
+    parser.add_argument('-t', '--mysql_table', type=str, default='log_table')
+    parser.add_argument('-z', '--timezone_hours', type=int, default=9)
+
+    args = parser.parse_args()
+    pprint(args.__dict__)
+    ingester = Ingester(**args.__dict__)
+    ingester.start()
+
+
+if __name__ == '__main__':
+    main()
